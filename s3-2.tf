c8a6d83c875302768f9c659fb25307167b42fa71
Tal Amsalem <100066758+tamsalem@users.noreply.github.com>
2022-04-14 08:40:38 +0300

    Create s3-2.tf

---
+++ s3-2.tf
@@ -0,0 +1,20 @@
+
+   
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "private"
+  force_destroy = true
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+    }, {
+    git_commit = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file   = "terraform/aws/s3.tf"
+    }, {
+    yor_trace = "116ba5c8-7ecd-432b-803c-f761046342d4"
+  })
+}
